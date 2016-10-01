2f93d8f63a7973f0d5f7f0a348f0925eae6937eb
Byron Weiss <byron.weiss@gmail.com>
2016-10-01 17:20:16 +0000

    sinatra up and config working

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 
+gem 'sinatra-contrib'
+
 gem 'httparty'
 
-gem 'dotenv'
+gem 'wit'
 
-
-
+gem 'tilt',  '~>2.0.2'
--- bin/chatbot.rb
+++ bin/chatbot.rb
@@ -4,18 +4,31 @@ require 'bundler/setup'
 require 'uri'
 require 'open-uri'
 require 'httparty'
+require 'wit'
+require "sinatra/config_file"
+
+config_file '../config.yml'
 
 get '/' do
-  erb :'index.html'
-end
-
-#enable :sessions
-post '/' do
-  content_type('application/json')
-
+  @greeting = settings.greeting
+  p @greeting
+  send_file File.expand_path('index.html', settings.public_folder)
 end
 
 
-  
+# actions = {
+#   send: -> (request, response) {
+#     puts("sending... #{response['text']}")
+#   },
+#   my_action: -> (request) {
+#     return request['context']
+#   },
+# }
+
+# client = Wit.new(access_token: access_token, actions: actions)
+
+
+# rsp = client.message('what is the weather in London?')
+# puts("Yay, got Wit.ai response: #{rsp}")  
 
 
